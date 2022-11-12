84a1ff06e20b1ab941663688c0ace42ea4224fd3
agnarrarendelle <danny20001102@gmail.com>
2022-11-12 17:31:47 +0800

    added missing modules

--- src/debugger.rs
+++ src/debugger.rs
@@ -3,18 +3,30 @@ use crate::debugger_command::DebuggerCommand;
 use crate::inferior::{Inferior, Status};
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
-
+use crate::dwarf_data::{DwarfData, Error as DwarfError};
 pub struct Debugger {
     target: String,
     history_path: String,
     readline: Editor<()>,
     inferior: Option<Inferior>,
+    debug_data:DwarfData
 }
 
 impl Debugger {
     /// Initializes the debugger.
     pub fn new(target: &str) -> Debugger {
         // TODO (milestone 3): initialize the DwarfData
+        let debug_data = match DwarfData::from_file(target) {
+            Ok(val) => val,
+            Err(DwarfError::ErrorOpeningFile) => {
+                println!("Could not open file {}", target);
+                std::process::exit(1);
+            }
+            Err(DwarfError::DwarfFormatError(err)) => {
+                println!("Could not debugging symbols from {}: {:?}", target, err);
+                std::process::exit(1);
+            }
+        };
 
         let history_path = format!("{}/.deet_history", std::env::var("HOME").unwrap());
         let mut readline = Editor::<()>::new();
@@ -26,6 +38,7 @@ impl Debugger {
             history_path,
             readline,
             inferior: None,
+            debug_data
         }
     }
 
